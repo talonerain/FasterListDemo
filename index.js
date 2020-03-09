64a1536cb03054211835e0cfef6c621f9360d3b4
lsy <sydugu@gmail.com>
2020-03-09 09:22:31 +0800

    SectionList

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { name as appName } from './app.json';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation'
 import FlatListDemo from './pages/FlatListDemo';
+import SectionListDemo from './pages/SectionListDemo';
 
 const AppRoot = createStackNavigator({
   App: {
@@ -16,6 +17,12 @@ const AppRoot = createStackNavigator({
     screen: FlatListDemo,
     navigationOptions: {
       title: 'FlatListDemo',
+    }
+  },
+  SectionListDemo: {
+    screen: SectionListDemo,
+    navigationOptions: {
+      title: 'SectionListDemo'
     }
   }
 });
